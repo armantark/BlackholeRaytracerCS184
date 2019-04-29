6e5c12d2bd9562f97c0ea6716a03d5124c9b0d9c
armantark <armantarkhanian@berkeley.edu>
2019-04-28 21:23:28 -0700

    Finished spectrum class

---
+++ src/Spectrum.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from math import sqrt
+
+@dataclass
+class Spectrum:
+    r: float = 0
+    g: float = 0
+    b: float = 0
+
+    def __add__(self, other):
+        return Spectrum(self.r + other.r, self.g + other.g, self.b + other.b)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        return Spectrum(self.r - other.r, self.g - other.g, self.b - other.b)
+
+    def __rsub__(self, other):
+        raise RuntimeError("???")
+
+    def __mul__(self, other):
+        return Spectrum(self.r * other.r, self.g * other.g, self.b * other.b)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __truediv__(self, other):
+        return Spectrum(self.r / other.r, self.g / other.g, self.b / other.b)
+
+    def illum(self):
+        return 0.2126 * self.r + 0.7152 * self.g + 0.0722 * self.b
+
