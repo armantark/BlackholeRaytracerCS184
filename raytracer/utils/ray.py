e5a6801876f474be5cd737bd5fc0fce327baa9e3
armantark <armantarkhanian@berkeley.edu>
2019-04-29 13:06:14 -0700

    finished the ray class

--- raytracer/utils/ray.py
+++ raytracer/utils/ray.py
@@ -1,2 +1,21 @@
+from vector import *
+
+@dataclass
 class Ray:
-    pass
+    depth: int = 0
+
+    o: Vector = Vector(0, 0 ,0)
+    d: Vector = Vector(0, 0, 0)
+    min_t: float = 0
+    max_t: float = float('inf')
+
+    inv_d: Vector = Vector(0,0,0)
+
+    def at_time(self, t):
+        return self.o + self.t * self.d
+
+    #may not be necessary but I'm putting this in here for the sake of completion later if necessary
+    def transform_by(self, t):
+        raise NotImplementedError()
+
+
