96ae4ef959c1f15c61a9f91ba4d095957a0b6ee6
armantark <armantarkhanian@berkeley.edu>
2019-05-09 17:51:14 -0700

    binary renders

--- raytracer/__main__.py
+++ raytracer/__main__.py
@@ -33,30 +33,40 @@ def normalize_vector(v: np.array) -> np.array:
 # @plac.annotation
 @jit
 def main():
-    disk_direction = normalize_vector(np.array([0,1,0]))
+    disk_direction = normalize_vector(np.array([,1,0]))
+    disk2_direction = normalize_vector(np.array([0,1,0]))
 
     world_radius = 50000
 
     objects_list = []
 
-    black_hole = BlackHole(mass=1.989e30, origin=np.array([world_radius] * 3))
+    black_hole = BlackHole(mass=1.989e30, origin=np.array([world_radius - 10000, world_radius, world_radius]))
     disk = Disk(
-        origin=np.array([world_radius] * 3),
+        origin=np.array([world_radius - 10000, world_radius, world_radius]),
         normal=disk_direction,
         inner_radius=black_hole.radius * 2.6,
         outer_radius=black_hole.radius * 6
     )
+    # black_hole2 = BlackHole(mass=1.989e30, origin=np.array([world_radius + 10000, world_radius, world_radius]))
+    # disk2 = Disk(
+    #     origin=np.array([world_radius + 10000, world_radius, world_radius]),
+    #     normal=disk_direction,
+    #     inner_radius=black_hole.radius * 2.6,
+    #     outer_radius=black_hole.radius * 6
+    # )
     bg = Background()
 
     objects_list.append(black_hole)
+    # objects_list.append(black_hole2)
     objects_list.append(disk)
+    # objects_list.append(disk2)
     # objects_list.extend(generate_stars(10, world_radius))
     objects_list.append(bg)
     # disk = Disk()
     world = World(objects=objects_list, size=np.array([world_radius * 2] * 3))
     camera = Camera(resolution=(300, 200), fov=(1, 0.66), position=np.array([world_radius, world_radius, 0]))
     image = render(world, camera)
-    image.save('test_render_010.png')
+    image.save('test_render_binary_disks.png')
 
 
 if __name__ == '__main__':
